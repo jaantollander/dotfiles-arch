2a60378500f6f12181f3c8a0699b97108d76ba6e
Jaan Tollander de Balsch <jaan@hey.com>
2022-01-04 11:16:54 +0000

    Neovim configs

--- install.sh
+++ install.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-########
-# nvim #
-########
-
-mkdir -p "$XDG_CONFIG_HOME/nvim"
-mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
-
-ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
-
 #######
 # X11 #
 #######
+
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
@@ -51,5 +43,25 @@ ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
 # Git #
 #######
 
-mkdir - p "$XDG_CONFIG_HOME/git"
+mkdir -p "$XDG_CONFIG_HOME/git"
 ln -sf "$DOTFILES/git/config" "$XDG_CONFIG_HOME/git/config"
+
+########
+# nvim #
+########
+
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
+
+ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
+
+# install neovim plugin manager
+[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
+    && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
+ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+
+# Install (or update) all the plugins
+nvim --noplugin +PlugUpdate +qa
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,3 +1,12 @@
+" Plugins
+call plug#begin("$XDG_CONFIG_HOME/nvim/plugged")
+    Plug 'chrisbra/csv.vim'
+    " Plug 'moll/vim-bbye'
+    " Plug 'simeji/winresizer'
+    Plug 'junegunn/fzf.vim'
+    Plug 'simnalamburt/vim-mundo'
+call plug#end()
+
 set clipboard+=unnamedplus
 
 noremap <Up> <Nop>
@@ -28,3 +37,19 @@ set shiftwidth=4
 
 " Show substitution
 set inccommand=nosplit
+
+nnoremap <space> <nop>
+let mapleader = "\<space>"
+
+nnoremap <leader>bn :bn<cr> ;buffer next
+nnoremap <leader>tn gt ;new tab
+
+" Config for chrisbra/csv.vim
+augroup filetype_csv
+    autocmd! 
+    autocmd BufRead,BufWritePost *.csv :%ArrangeColumn!
+    autocmd BufWritePre *.csv :%UnArrangeColumn
+augroup END
+
+" Config for fzf.vim (BONUS :D)
+nnoremap <leader>f :Files<cr>
