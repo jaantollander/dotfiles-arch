ac72a8f27927474d0bceb1a3cef4eb30a65f9974
Jaan Tollander de Balsch <jaan@hey.com>
2022-01-07 19:03:03 +0000

    i3, aliases, redshift

--- install_configs.sh
+++ install_configs.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-#######
-# X11 #
-#######
-
+## X11
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
-######
-# i3 #
-######
-
+## i3
 rm -rf "$XDG_CONFIG_HOME/i3"
 ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
-#######
-# Zsh #
-#######
-
+## Zsh
 mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
 ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
@@ -25,31 +16,19 @@ ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
-#########
-# Fonts #
-#########
-
+## Fonts
 mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
-#########
-# dunst #
-#########
-
+## dunst
 mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
 
-#######
-# Git #
-#######
-
+## Git
 mkdir -p "$XDG_CONFIG_HOME/git"
 ln -sf "$DOTFILES/git/config" "$XDG_CONFIG_HOME/git/config"
 
-########
-# nvim #
-########
-
+## nvim
 mkdir -p "$XDG_CONFIG_HOME/nvim"
 mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
@@ -66,11 +45,7 @@ ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.v
 # Install (or update) all the plugins
 nvim --noplugin +PlugUpdate +qa
 
-########
-# tmux #
-########
-
-
+## tmux
 mkdir -p "$XDG_CONFIG_HOME/tmux"
 ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 
@@ -78,9 +53,10 @@ ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 && git clone https://github.com/tmux-plugins/tpm \
 "$XDG_CONFIG_HOME/tmux/plugins/tpm"
 
-##############
-# GitHub CLI #
-##############
-
+## GitHub CLI
 mkdir -p "$XDG_CONFIG_HOME/gh"
 ln -sf "$DOTFILES/gh/config.yml" "$XDG_CONFIG_HOME/gh/config.yml"
+
+## redshift
+mkdir -p "$XDG_CONFIG_HOME/redshift"
+ln -sf "$DOTFILES/redshift/redshift.conf" "$XDG_CONFIG_HOME/redshift/redshift.conf"
