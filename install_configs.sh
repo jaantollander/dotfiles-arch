63235ab2fea6c155f83d349308c94c1d0418e315
Jaan Tollander de Balsch <jaan@hey.com>
2022-01-08 14:55:26 +0000

    removed dots from filenames, improved install

--- install_configs.sh
+++ install_configs.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 ## X11
-rm -rf "$XDG_CONFIG_HOME/X11"
-ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
+mkdir -p "$XDG_CONFIG_HOME/X11"
+ln -sf "$DOTFILES/X11/xinitrc" "$XDG_CONFIG_HOME/X11/.xinitrc"
+ln -sf "$DOTFILES/X11/Xresources" "$XDG_CONFIG_HOME/X11/.Xresources"
+ln -sf "$DOTFILES/X11/monitor_external.sh" "$XDG_CONFIG_HOME/X11/monitor_external.sh"
+ln -sf "$DOTFILES/X11/monitor_laptop.sh" "$XDG_CONFIG_HOME/X11/monitor_laptop.sh"
 
 ## i3
-rm -rf "$XDG_CONFIG_HOME/i3"
-ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
+mkdir -p "$XDG_CONFIG_HOME/i3"
+ln -sf "$DOTFILES/i3/config" "$XDG_CONFIG_HOME/i3/config"
+ln -sf "$DOTFILES/i3/i3status.conf" "$XDG_CONFIG_HOME/i3/i3status.conf"
 
 ## Zsh
 mkdir -p "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
-ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES/zsh/zshrc" "$XDG_CONFIG_HOME/zsh/.zshrc"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
