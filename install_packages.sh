a752322a5019e6812a05aee1ca9dc51b2d974ecc
Jaan Tollander de Balsch <jaan@hey.com>
2022-01-11 09:48:40 +0000

    added new packages

--- install_packages.sh
+++ install_packages.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+sudo pacman --sync --refresh --sysupgrade 
+
 sudo pacman -S man man-pages tldr
-sudo pacman -S openssh networkmanager #openvpn
+sudo pacman -S networkmanager openssh openvpn
+sudo pacman -S rsync scrot htop tree
 
 source $DOTFILES/yay/packages.sh
 source $DOTFILES/X11/packages.sh
 source $DOTFILES/i3/packages.sh
+source $DOTFILES/redshift/packages.sh
 source $DOTFILES/dunst/packages.sh
 source $DOTFILES/zsh/packages.sh
 source $DOTFILES/tmux/packages.sh
 source $DOTFILES/nvim/packages.sh
 source $DOTFILES/git/packages.sh
 source $DOTFILES/gh/packages.sh
-source $DOTFILES/redshift/packages.sh
 
-sudo pacman -S rsync scrot htop tree
+sudo pacman -S firefox brave-bin
+sudo pacman -S keepassxc
+sudo pacman -S hledger hledger-ui
+
 #pacman -S nodejs yarn
-sudo pacman -S firefox
+#pacman -S julia
