3821f335d090736c66a43e83631b95d33fa2a10f
Jaan Tollander de Balsch <jaan@hey.com>
2022-01-18 16:17:22 +0200

    refactoring

--- X11/config.sh
+++ X11/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p "$XDG_CONFIG_HOME/X11"
-ln -sf "$DOTFILES/X11/xinitrc" "$XDG_CONFIG_HOME/X11/.xinitrc"
-ln -sf "$DOTFILES/X11/xresources" "$XDG_CONFIG_HOME/X11/.Xresources"
-ln -sf "$DOTFILES/X11/monitor_external.sh" "$XDG_CONFIG_HOME/X11/monitor_external.sh"
-ln -sf "$DOTFILES/X11/monitor_laptop.sh" "$XDG_CONFIG_HOME/X11/monitor_laptop.sh"
+ln -sf "$DOTFILES/X11/config/xinitrc" "$XDG_CONFIG_HOME/X11/.xinitrc"
+ln -sf "$DOTFILES/X11/config/xresources" "$XDG_CONFIG_HOME/X11/.Xresources"
+ln -sf "$DOTFILES/X11/config/monitor_external.sh" "$XDG_CONFIG_HOME/X11/monitor_external.sh"
+ln -sf "$DOTFILES/X11/config/monitor_laptop.sh" "$XDG_CONFIG_HOME/X11/monitor_laptop.sh"
