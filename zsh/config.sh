9566151dc7b42f1b71411e7e5437a6b2339c8e35
Jaan Tollander de Balsch <jaan@hey.com>
2022-01-20 15:03:34 +0200

    improve zsh configs

--- zsh/config.sh
+++ zsh/config.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/config/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES/zsh/config/zprofile" "$XDG_CONFIG_HOME/zsh/.zprofile"
 ln -sf "$DOTFILES/zsh/config/zshrc" "$XDG_CONFIG_HOME/zsh/.zshrc"
 ln -sf "$DOTFILES/zsh/config/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 ln -sf "$DOTFILES/zsh/config/dircolors" "$XDG_CONFIG_HOME/zsh/dircolors"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/config/external" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/config/scripts.sh" "$XDG_CONFIG_HOME/zsh/scripts.sh"
+
+# If login shell is not zsh change it to zsh.
+if [[ $SHELL =~ "zsh" ]]; then
+    echo "Zsh is already the login shell."
+else
+    echo "Change login shell to zsh."
+    # Initiates a password prompt.
+    chsh -s $(which zsh)
+fi
