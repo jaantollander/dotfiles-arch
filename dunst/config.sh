6effefbcbe123abef7ccd1ee89d36e1cf10abf06
Jaan Tollander de Balsch <jaan@hey.com>
2022-01-23 14:55:22 +0200

    improved i3 config

--- dunst/config.sh
+++ dunst/config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/config/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
+ln -sf $DOTFILES/dunst/config/i3.conf $XDG_CONFIG_HOME/dunst/i3.conf
--- rofi/config.sh
+++ rofi/config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p "$XDG_CONFIG_HOME/rofi"
 ln -sf "$DOTFILES/rofi/config/config.rasi" "$XDG_CONFIG_HOME/rofi/config.rasi"
+ln -sf $DOTFILES/rofi/config/i3.conf $XDG_CONFIG_HOME/rofi/i3.conf
