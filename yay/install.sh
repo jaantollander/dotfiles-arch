8a69785b87dc62961b7a9a116deb1f88bb989863
Jaan Tollander de Balsch <jaan@hey.com>
2022-02-05 13:25:40 +0200

    improve yay install script

--- yay/install.sh
+++ yay/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-curl -O "https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz" -o "/tmp"
-tar -xvf "/tmp/yay.tar.gz" --directory="/tmp/"
-cd "/tmp/yay/"
+OUTDIR=$(mktemp -d)
+git clone https://aur.archlinux.org/yay.git $OUTDIR
+cd $OUTDIR
 makepkg -s
 sudo pacman -U "yay-*.pkg.tar.zst"
-cd -
