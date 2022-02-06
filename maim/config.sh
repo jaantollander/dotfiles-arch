e4839dfca0d179620c050e8749f7c2eb92da283b
Jaan Tollander de Balsch <jaan@hey.com>
2022-02-06 13:48:49 +0200

    screenshots directory

--- maim/config.sh
+++ maim/config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p $HOME/screenshot
+mkdir -p $HOME/screenshots
 mkdir -p $XDG_CONFIG_HOME/maim
 ln -sf $DOTFILES/maim/config/i3.conf $XDG_CONFIG_HOME/maim/i3.conf
--- maim/config/scripts.sh
+++ maim/config/scripts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 screenshot_display() {
-    mkdir -p $HOME/screenshot
-    maim $HOME/screenshot/$(date -Iseconds).png 
+    mkdir -p $HOME/screenshots
+    maim $HOME/screenshots/$(date -Iseconds).png 
 }
 
 screenshot_activewindow() {
-    mkdir -p $HOME/screenshot
-    maim -i $(xdottool getactivewindow) $HOME/screenshot/(date -Iseconds).png
+    mkdir -p $HOME/screenshots
+    maim -i $(xdottool getactivewindow) $HOME/screenshots/(date -Iseconds).png
 }
