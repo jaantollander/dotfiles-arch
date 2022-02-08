97401cd452aed658c35cc3bd0be02b958b81f2d1
Jaan Tollander de Balsch <jaan@hey.com>
2022-02-08 14:42:51 +0200

    improved lsd aliases

--- lsd/config/aliases.sh
+++ lsd/config/aliases.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 alias ls='lsd'
-alias l='lsd -l'
+alias l='lsd -l --total-size'
 alias la='lsd -A'
-alias ll='lsd -lA'
+alias ll='lsd -lA --total-size'
+
 alias lt='lsd --tree'
+alias lta='lsd --tree -A'
+alias ltl='lsd --tree -l --total-size'
+alias ltla='lsd --tree -lA --total-size'
