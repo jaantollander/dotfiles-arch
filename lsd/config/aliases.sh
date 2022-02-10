050a8fa0b8f7e94ba473c29e8a7458c2c1d3b075
Jaan Tollander de Balsch <jaan@hey.com>
2022-02-10 12:13:23 +0200

    total size and date formats to configs

--- lsd/config/aliases.sh
+++ lsd/config/aliases.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-alias ls='lsd --total-size --date="+%Y-%m-%d"'
+alias ls='lsd'
 
 alias l='ls -l'
 alias la='ls -A'
 alias ll='ls -lA'
 
-alias lt='ls --tree'
+alias lt='lsd --tree'
 alias lta='lt -A'
 alias ltl='lt -l'
 alias ltla='lt -lA'
