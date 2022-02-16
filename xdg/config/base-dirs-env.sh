d6fb56cfa8ae694f32fb48f95899cf1bece57f52
Jaan Tollander de Balsch <jaan@hey.com>
2022-02-16 16:01:23 +0200

    added base and user directories

--- xdg/config/base-dirs-env.sh
+++ xdg/config/base-dirs-env.sh
@@ -1,6 +1,9 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_LOCAL_HOME="$HOME/.local"
+
 export XDG_DATA_HOME="$XDG_LOCAL_HOME/share"
 export XDG_STATE_HOME="$XDG_LOCAL_HOME/state"
+export XDG_BIN_HOME="$XDG_LOCAL_HOME/bin"
+
 export XDG_SCRIPTS_HOME="$XDG_CONFIG_HOME/scripts"
--- xdg/config/base-dirs-mk.sh
+++ xdg/config/base-dirs-mk.sh
@@ -2,6 +2,9 @@
 mkdir -p $XDG_CONFIG_HOME \
          $XDG_CACHE_HOME \
          $XDG_LOCAL_HOME
+
 mkdir -p $XDG_DATA_HOME \
          $XDG_STATE_HOME \
-         $XDG_SCRIPTS_HOME
+         $XDG_BIN_HOME
+
+mkdir -p $XDG_SCRIPTS_HOME
