52f2042cd452b9b0eff2f3a20124314a9aec86e4
Jaan Tollander de Balsch <jaan@hey.com>
2022-03-05 15:47:23 +0200

    ssh configs

---
+++ module/ssh/config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p $HOME/.ssh
+chmod u=rwx,g=,o=r $HOME/.ssh 
+
+# System-wide configuration
+sudo mkdir -p /etc/ssh
+sudo chmod u=rwx,g=rx,o=rx /etc/ssh
+sudo cp $DOTFILES/module/ssh/config/ssh_config /etc/ssh/ssh_config
+sudo chmod u=rw,g=r,o=r /etc/ssh/ssh_config
+
+# User's configuration. Create if doesn't exist.
+touch $HOME/.ssh/config
+chmod u=rw,g=,o= $HOME/.ssh/config
