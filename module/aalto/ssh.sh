e146400e7ab38a38ba5adf67cca3b4f9d665faf0
Jaan Tollander de Balsch <jaan@hey.com>
2022-03-14 09:43:11 +0200

    ssh configs, modularity

--- module/aalto/ssh.sh
+++ module/aalto/ssh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 COMMENT=$1
 ALGORITHM="ed25519"
-SUFFIX="github"
+SUFFIX="aalto"
 IDFILE="$HOME/.ssh/id_${ALGORITHM}_${SUFFIX}"
 
 if [[ ! -f $IDFILE ]]; then
     ssh-keygen -t $ALGORITHM -f $IDFILE -C $COMMENT
-    echo "You should copy the public key ${IDFILE}.pub to GitHub https://github.com/settings/keys"
+    echo "run \`ssh-copy-id -i $IDFILE.pub <username>@kosh.aalto.fi\` to add the public key to aalto servers."
 fi
--- module/openssh/config.sh
+++ module/openssh/config.sh
@@ -5,12 +5,12 @@ chmod u=rwx,g=,o=r $HOME/.ssh
 # System-wide configuration
 #sudo mkdir -p /etc/ssh
 #sudo chmod u=rwx,g=rx,o=rx /etc/ssh
-#sudo cp $DOTFILES/module/ssh/config/ssh_config /etc/ssh/ssh_config
+#sudo cp $DOTFILES/openssh/config/ssh_config /etc/ssh/ssh_config
 #sudo chmod u=rw,g=r,o=r /etc/ssh/ssh_config
 
 # User's configuration. Create if doesn't exist.
 touch $HOME/.ssh/config
 chmod u=rw,g=,o= $HOME/.ssh/config
 
-mkdir -p $XDG_SCRIPTS_HOME
-ln -sf $DOTFILES/ssh/config/start-ssh-agent.sh $XDG_SCRIPTS_HOME/start-ssh-agent.sh
+#mkdir -p $XDG_SCRIPTS_HOME
+#ln -sf $DOTFILES/openssh/config/start-ssh-agent.sh $XDG_SCRIPTS_HOME/start-ssh-agent.sh
