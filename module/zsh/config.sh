0bb3e200a10aacbd269c90df193beb8d2932b8f0
Jaan Tollander de Balsch <jaan@hey.com>
2022-03-17 12:36:19 +0200

    restructured zsh configuration

--- module/zsh/config.sh
+++ module/zsh/config.sh
@@ -3,13 +3,8 @@ mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/config/zshenv" "$HOME/.zshenv"
 ln -sf "$DOTFILES/zsh/config/zprofile" "$XDG_CONFIG_HOME/zsh/.zprofile"
 ln -sf "$DOTFILES/zsh/config/zshrc" "$XDG_CONFIG_HOME/zsh/.zshrc"
-ln -sf "$DOTFILES/zsh/config/dircolors" "$XDG_CONFIG_HOME/zsh/dircolors"
-
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/config/external" "$XDG_CONFIG_HOME/zsh"
-
-ln -sf "$DOTFILES/zsh/config/aliases.sh" "$XDG_SCRIPTS_HOME/zsh-aliases.sh"
-ln -sf "$DOTFILES/zsh/config/scripts.sh" "$XDG_SCRIPTS_HOME/zsh-scripts.sh"
 
 # If login shell is not zsh change it to zsh.
 if [[ $SHELL =~ "zsh" ]]; then
