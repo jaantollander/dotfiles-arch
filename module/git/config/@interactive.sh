da8868835af65818b55a5d10dc5350cd9d6098af
Jaan Tollander de Balsch <jaan@hey.com>
2022-04-17 13:13:34 +0300

    default branch to main, new aliases

--- module/git/config/@interactive.sh
+++ module/git/config/@interactive.sh
@@ -1,5 +1,8 @@
 alias gs='git status'
 alias gd='git diff'
+alias gdc='git diff --cached'
 alias ga='git add'
 alias gc='git commit'
 alias gp='git push'
+alias gpl='git pull'
+alias gch='git checkout'
