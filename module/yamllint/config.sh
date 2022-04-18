acd03865709416aa17285c906e0057202420bdb9
Jaan Tollander de Balsch <jaan@hey.com>
2022-04-18 12:33:48 +0300

    improved yamllint config

--- module/yamllint/config.sh
+++ module/yamllint/config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p $XDG_CONFIG_HOME/yamllint
-ln -sf $DOTFILES/yamllint/config/strict.yaml $XDG_CONFIG_HOME/yamllint/config
+ln -sf $DOTFILES/yamllint/config/config.yaml $XDG_CONFIG_HOME/yamllint/config
