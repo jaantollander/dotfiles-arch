09507538faa08fee98d1e3389406421a95a57a6a
Jaan Tollander de Balsch <jaan@hey.com>
2022-04-23 11:58:11 +0300

    improved lsd config

--- module/lsd/config.sh
+++ module/lsd/config.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-mkdir -p $XDG_CONFIG_HOME/lsd
-ln -sf $DOTFILES/lsd/config/config.yaml $XDG_CONFIG_HOME/lsd/config.yaml
+#!/usr/bin/env sh
+mkdir -p "$XDG_CONFIG_HOME/lsd"
+ln -sf "$DOTFILES/lsd/config/config.yaml" "$XDG_CONFIG_HOME/lsd/config.yaml"
 
-mkdir -p $XDG_CONFIG_HOME/shell/interactive
-ln -sf $DOTFILES/lsd/config/@interactive.sh $XDG_CONFIG_HOME/shell/interactive/lsd.sh
+mkdir -p "$XDG_CONFIG_HOME/shell/interactive"
+ln -sf "$DOTFILES/lsd/config/@interactive.sh" "$XDG_CONFIG_HOME/shell/interactive/lsd.sh"
--- module/lsd/config/@interactive.sh
+++ module/lsd/config/@interactive.sh
@@ -1,8 +1,10 @@
-alias ls='lsd'
-alias l='ls -l'
-alias la='ls -A'
-alias ll='ls -lA'
-alias lt='lsd --tree'
-alias lta='lt -A'
-alias ltl='lt -l'
-alias ltla='lt -lA'
+if [ "$(command -v "lsd")" ]; then
+    alias ls='lsd'
+    alias l='ls -l'
+    alias la='ls -A'
+    alias ll='ls -lA'
+    alias lt='lsd --tree'
+    alias lta='lt -A'
+    alias ltl='lt -l'
+    alias ltla='lt -lA'
+fi
