9312619a22b362a65de73ea3d84750df88de53c9
Jaan Tollander de Balsch <jaan@hey.com>
2022-04-24 12:40:15 +0300

    moved xrandr configs to thinkpad t480s module

---
+++ module/thinkpad_t480s/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p "$XDG_CONFIG_HOME/X11/xrandr"
+ln -sf "$DOTFILES/thinkpad_t480s/config/xrandr/external.sh" "$XDG_CONFIG_HOME/X11/xrandr/external.sh"
+ln -sf "$DOTFILES/thinkpad_t480s/config/xrandr/laptop.sh" "$XDG_CONFIG_HOME/X11/xrandr/laptop.sh"
+
+mkdir -p "$XDG_CONFIG_HOME/i3/include"
+ln -sf "$DOTFILES/thinkpad_t480s/config/i3.conf" "$XDG_CONFIG_HOME/i3/include/xrandr.conf"
