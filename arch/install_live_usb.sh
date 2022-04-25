3289b8f1a6f55b72b3168b1db2986d27e33a407a
Jaan Tollander de Balsch <jaan@hey.com>
2022-04-25 15:31:51 +0300

    updated live usb install script

--- arch/install_live_usb.sh
+++ arch/install_live_usb.sh
@@ -1,26 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-## Usage
-# bash install_live_usb.sh <arc_iso_path> <usb_device_path>
-
-## Resources
+# Resources
 # https://wiki.archlinux.org/title/USB_flash_installation_medium
 
 # Path to the Arch Linux ISO file.
 # Example: "~/downloads/archlinux-<version>-86_64.iso"
-arch_iso=$1
+ARCHISO=$1
 
 # Path to the USB device.
 # Example: `/dev/sdb`
 # Use `lsblk` command to list block devices.
-usb_device=$2
+USB_DEVICE=$2
 
 # Unmount all partitions of the USB device if they are mounted
-umount ${usb_device}* 2> /dev/null
+sudo umount "${USB_DEVICE}"*
+
+# Wipe existing filesystem from the USB device
+sudo wipefs "$USB_DEVICE"
 
 # Make FAT32 filesystem on the USB device
-mkfs.fat -F 32 $usb_device 2> /dev/null
+sudo mkfs.fat -F 32 "$USB_DEVICE"
 
 # Copy the Arch Linux ISO file to the USB device.
-cat $arch_iso > $usb_device
+sudo dd if="$ARCHISO" of="$USB_DEVICE" bs=4M status=progress
 
+# Finally, eject the USB device.
+sudo eject "$USB_DEVICE"
