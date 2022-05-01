2293210046353f9f5f0461e024c37936816bb0fc
Jaan Tollander de Balsch <jaan@hey.com>
2022-05-01 15:26:29 +0300

    colored ctrl+t

--- module/fzf/config/@env.sh
+++ module/fzf/config/@env.sh
@@ -1,3 +1,4 @@
 # shellcheck shell=sh
-export FZF_DEFAULT_COMMAND="fd --type file --hidden --exclude .git"
+export FZF_DEFAULT_COMMAND="fd --type file --hidden --color=always --exclude .git"
+export FZF_DEFAULT_OPTS="--ansi"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
