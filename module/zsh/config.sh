fb8f172dca20480e883bac1fc379dcc83a7f1b1b
Jaan Tollander de Balsch <jaan@hey.com>
2022-05-01 12:37:21 +0300

    improved linking

--- module/zsh/config.sh
+++ module/zsh/config.sh
@@ -5,8 +5,11 @@ ln -sf "$DOTMODULE/zsh/config/@login.zsh" "$XDG_CONFIG_HOME/zsh/.zprofile"
 ln -sf "$DOTMODULE/zsh/config/@interactive.zsh" "$XDG_CONFIG_HOME/zsh/.zshrc"
 ln -sf "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
 
-rm -rf "$XDG_CONFIG_HOME/zsh/external"
-ln -sf "$DOTMODULE/zsh/config/external" "$XDG_CONFIG_HOME/zsh"
+mkdir -p "$XDG_CONFIG_HOME/zsh/external"
+ln -sf "$DOTMODULE/zsh/config/external/bd.zsh" "$XDG_CONFIG_HOME/zsh/external/bd.zsh"
+ln -sf "$DOTMODULE/zsh/config/external/completion.zsh" "$XDG_CONFIG_HOME/zsh/external/completion.zsh"
+ln -sf "$DOTMODULE/zsh/config/external/cursor_mode.zsh" "$XDG_CONFIG_HOME/zsh/external/cursor_mode"
+ln -sf "$DOTMODULE/zsh/config/external/prompt_purification_setup.zsh" "$XDG_CONFIG_HOME/zsh/external/prompt_purification_setup"
 
 # If login shell is not zsh change it to zsh.
 if test "$SHELL" = "$(command -v zsh)"; then
