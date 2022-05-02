7a3d16324dadc39d1f376bc350182589691d4f93
Jaan Tollander de Balsch <jaan@hey.com>
2022-05-02 15:32:17 +0300

    shellcheckrc as arguments

--- shellcheck.sh
+++ shellcheck.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 # Shellsheck all files with extensions ".sh" and ".bash".
-find . -name '*.sh' -type 'f' -print0 | xargs -0 shellcheck
-find . -name '*.bash' -type 'f' -print0 | xargs -0 shellcheck
+ARGS="--exclude=SC1090,SC1091 --enable=deprecate-which --enable=avoid-nullary-conditions"
+# shellcheck disable=SC2086
+find . -name '*.sh' -type 'f' -print0 | xargs -0 shellcheck $ARGS
+# shellcheck disable=SC2086
+find . -name '*.bash' -type 'f' -print0 | xargs -0 shellcheck $ARGS
