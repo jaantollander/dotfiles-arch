2daab9cdf0b8e46922ad0b37ebf9ae2601694ae2
Jaan Tollander de Balsch <jaan@hey.com>
2022-05-03 11:07:25 +0300

    better comments

--- arch/install_live_usb.sh
+++ arch/install_live_usb.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env sh
 
-# Resources
-# https://wiki.archlinux.org/title/USB_flash_installation_medium
+# Install Arch Linux Live USB
+#
+# USAGE
+#   install_live_usb.sh <archiso> <usb-device>
+#
+# EXAMPLE
+#   install_live_usb.sh archlinux-<version>-86_64.iso /dev/sdb
+#
+# LINKS
+#   https://wiki.archlinux.org/title/USB_flash_installation_medium
 
 # Path to the Arch Linux ISO file.
-# Example: "~/downloads/archlinux-<version>-86_64.iso"
 ARCHISO=$1
 
 # Path to the USB device.
-# Example: `/dev/sdb`
-# Use `lsblk` command to list block devices.
 USB_DEVICE=$2
 
-# Unmount all partitions of the USB device if they are mounted
+# Unmount all partitions of the USB device if they are mounted.
 sudo umount "${USB_DEVICE}"*
 
-# Wipe existing filesystem from the USB device
+# Wipe existing filesystem from the USB device.
 sudo wipefs "$USB_DEVICE"
 
-# Make FAT32 filesystem on the USB device
+# Make FAT32 filesystem on the USB device.
 sudo mkfs.fat -F 32 "$USB_DEVICE"
 
 # Copy the Arch Linux ISO file to the USB device.
